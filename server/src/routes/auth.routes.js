bf4b48b6a0b53714b1579b1d72c79f9bce4e6769
HarshPatel2704 <hp210522@gmail.com>
2026-07-17 21:41:03 +0530

    Update auth.routes.js

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -10,11 +10,10 @@ import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// Public Routes
 router.post("/register", register);
 router.post("/login", login);
 
-// Protected Routes
+
 router.get("/profile", verifyToken, profile);
 router.post("/logout", verifyToken, logout);
 
